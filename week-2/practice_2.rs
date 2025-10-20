ee5c00205c91874717c3a3f4f93416085a38c8fd
ochudi <chukwudi.ofoma@pau.edu.ng>
2025-10-20 16:33:35 +0100

    Add macro example

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!(); // prints:
+    println!("Hello!"); //prints: Hello!
+    println!("format {} arguments.", "some"); //prints: Format some arguments.
+    println!("My name is {}. I am the lecturer of COS101.", "Chukwudi Ofoma."); //prints: My name is Chukwudi Ofoma. I am the lecturer of COS101.
+}
